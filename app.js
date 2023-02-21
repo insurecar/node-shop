0a653fd73a81b8e31caa0a4b6fc9e7a7a6b974d5
rostyslav-shyian-exa <rostyslav.shyian@exabeam.com>
2023-02-21 10:22:52 +0200

    finished sprint 5

---
+++ app.js
@@ -0,0 +1,37 @@
+// const http = require("http");
+// const fs = require("fs");
+
+// http
+//   .createServer(function (req, resp) {
+//     console.log(req.url);
+//     console.log(req.method);
+//     console.log(req.headers["user-agent"]);
+//     resp.setHeader("Content-Type", "text/html;cherset='utf-8;");
+
+//     if (req.url === "/") {
+//       resp.end("Main <b>Hello</b>");
+//     } else if (req.url === "/cat") {
+//       resp.end("Category <h2>Hello</h2>");
+//     } else if (req.url === "/dat") {
+//       const data = fs.readFileSync("1.dat");
+//       console.log(data);
+//       resp.end(data);
+//     }
+//   })
+//   .listen(3008);
+const express = require("express");
+const app = express();
+app.use(express.static("public"));
+
+app.listen(3005, () => {
+  console.log("node express work on 3005");
+});
+
+app.get("/", function (req, resp) {
+  console.log("load /");
+  resp.render("index.html");
+});
+
+app.get("/cat", function (req, resp) {
+  resp.end("Hello i am cat");
+});
